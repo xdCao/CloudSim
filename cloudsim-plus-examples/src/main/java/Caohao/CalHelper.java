a338807ab3dde9d137d323606fa793405da58563
caohao <705083979@qq.com>
2018-11-02 20:08:20 +0800

    新的仿真数据

--- cloudsim-plus-examples/src/main/java/Caohao/CalHelper.java
+++ cloudsim-plus-examples/src/main/java/Caohao/CalHelper.java
@@ -4,10 +4,7 @@ import Caohao.entity.QosVm;
 import org.cloudbus.cloudsim.hosts.Host;
 import org.cloudbus.cloudsim.vms.Vm;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import static Caohao.Constants.VAR_THRESHOLD;
 
@@ -22,36 +19,36 @@ public class CalHelper {
     public static double calDistribution(Host host) {
 
         List<QosVm> vmList = host.getVmList();
-        if (vmList.size()==0){
+        if (vmList.size() == 0) {
             return 0.0;
-        }else {
-            double sum=0;
-            List<Double> qosList=new ArrayList<>();
-            for (QosVm vm:vmList){
-                sum+=vm.getQos();
+        } else {
+            double sum = 0;
+            List<Double> qosList = new ArrayList<>();
+            for (QosVm vm : vmList) {
+                sum += vm.getQos();
                 qosList.add(vm.getQos());
             }
-            double ava=sum/qosList.size();
-            double var=0;
-            for (QosVm vm:vmList){
-                var+=Math.pow(vm.getQos()-ava,2);
+            double ava = sum / qosList.size();
+            double var = 0;
+            for (QosVm vm : vmList) {
+                var += Math.pow(vm.getQos() - ava, 2);
             }
-            return var/vmList.size();
+            return var / vmList.size();
         }
 
     }
 
     //计算PM的QOS均值
-    public static double calAvaQos(Host host){
+    public static double calAvaQos(Host host) {
         List<QosVm> vmList = host.getVmList();
-        if (vmList.size()==0){
+        if (vmList.size() == 0) {
             return 0.0;
-        }else {
-            double sum=0;
-            for (QosVm vm:vmList){
-                sum+=vm.getQos();
+        } else {
+            double sum = 0;
+            for (QosVm vm : vmList) {
+                sum += vm.getQos();
             }
-            double ava=sum/vmList.size();
+            double ava = sum / vmList.size();
             return ava;
         }
     }
@@ -59,46 +56,46 @@ public class CalHelper {
     public static double calDistributionRemoveNext(Host host, QosVm vm) {
 
         List<QosVm> vmList = host.getVmList();
-        if (vmList.size()==0||vmList.size()==1){
+        if (vmList.size() == 0 || vmList.size() == 1) {
             return 0.0;
-        }else {
-            double sum=0;
-            List<Double> qosList=new ArrayList<>();
-            for (QosVm qosVm:vmList){
-                sum+=qosVm.getQos();
+        } else {
+            double sum = 0;
+            List<Double> qosList = new ArrayList<>();
+            for (QosVm qosVm : vmList) {
+                sum += qosVm.getQos();
                 qosList.add(qosVm.getQos());
             }
-            sum-=vm.getQos();
-            double ava=sum/(qosList.size()-1);
-            double var=0;
-            for (QosVm qosVm:vmList){
-                var+=Math.pow(qosVm.getQos()-ava,2);
+            sum -= vm.getQos();
+            double ava = sum / (qosList.size() - 1);
+            double var = 0;
+            for (QosVm qosVm : vmList) {
+                var += Math.pow(qosVm.getQos() - ava, 2);
             }
-            var-=Math.pow(vm.getQos()-ava,2);
-            return var/(vmList.size()-1);
+            var -= Math.pow(vm.getQos() - ava, 2);
+            return var / (vmList.size() - 1);
         }
 
     }
 
     public static double calDistributionAddNext(Host host, QosVm vm) {
         List<QosVm> vmList = host.getVmList();
-        if (vmList.size()==0){
+        if (vmList.size() == 0) {
             return 0.0;
-        }else {
-            double sum=0;
-            List<Double> qosList=new ArrayList<>();
-            for (QosVm qosVm:vmList){
-                sum+=qosVm.getQos();
+        } else {
+            double sum = 0;
+            List<Double> qosList = new ArrayList<>();
+            for (QosVm qosVm : vmList) {
+                sum += qosVm.getQos();
                 qosList.add(qosVm.getQos());
             }
-            sum+=vm.getQos();
-            double ava=sum/(qosList.size()+1);
-            double var=0;
-            for (QosVm qosVm:vmList){
-                var+=Math.pow(qosVm.getQos()-ava,2);
+            sum += vm.getQos();
+            double ava = sum / (qosList.size() + 1);
+            double var = 0;
+            for (QosVm qosVm : vmList) {
+                var += Math.pow(qosVm.getQos() - ava, 2);
             }
-            var+=Math.pow(vm.getQos()-ava,2);
-            return var/(vmList.size()+1);
+            var += Math.pow(vm.getQos() - ava, 2);
+            return var / (vmList.size() + 1);
         }
     }
 
@@ -125,9 +122,9 @@ public class CalHelper {
             .filter(vm -> !vm.isInMigration())
             .max(Comparator.comparingDouble(Vm::getCurrentRequestedTotalMips))
             .map(vm -> vm.getCurrentRequestedTotalMips());
-        if (aDouble.isPresent()){
-            return (getHostTotalRequestedMips(host)-aDouble.get())/host.getTotalMipsCapacity()<0.125;
-        }else {
+        if (aDouble.isPresent()) {
+            return (getHostTotalRequestedMips(host) - aDouble.get()) / host.getTotalMipsCapacity() < 0.125;
+        } else {
             return false;
         }
 //        return getHostCpuUtilizationPercentage(host)<0.125;
@@ -139,20 +136,20 @@ public class CalHelper {
         return getHostTotalAllocatedMips(host) / host.getTotalMipsCapacity();
     }
 
-    public static double getHostCpuPercentAfterDeallocate(Host host,Vm vm){
+    public static double getHostCpuPercentAfterDeallocate(Host host, Vm vm) {
 
         double percent = (getHostTotalAllocatedMips(host) - vm.getCurrentRequestedTotalMips()) / host.getTotalMipsCapacity();
 
-        return percent<0?0:percent;
+        return percent < 0 ? 0 : percent;
 
     }
 
 
-    public static double getHostTotalAllocatedMips(Host host){
+    public static double getHostTotalAllocatedMips(Host host) {
         List<Vm> vmList = host.getVmList();
-        double sum=0;
-        for (Vm vm:vmList){
-            sum+=host.getTotalAllocatedMipsForVm(vm);
+        double sum = 0;
+        for (Vm vm : vmList) {
+            sum += host.getTotalAllocatedMipsForVm(vm);
         }
         return sum;
     }
@@ -164,8 +161,8 @@ public class CalHelper {
     }
 
 
-    public static double getHostCpuUtilizationPercentageNext(Host host,Vm vm) {
-        return (getHostTotalAllocatedMips(host)+vm.getCurrentRequestedTotalMips()) / host.getTotalMipsCapacity();
+    public static double getHostCpuUtilizationPercentageNext(Host host, Vm vm) {
+        return (getHostTotalAllocatedMips(host) + vm.getCurrentRequestedTotalMips()) / host.getTotalMipsCapacity();
     }
 
 
